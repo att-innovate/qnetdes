04cfb7e70144391cba761aa97c244ba8c3153ab4
zacespinosa <zespinosa97@gmail.com>
2019-06-27 14:31:22 -0700

    add quantum-teleportation-demo

--- demos/quantum-teleportation-demo.py
+++ demos/quantum-teleportation-demo.py
@@ -7,6 +7,11 @@ from pyquil import Program
 from pyquil.gates import *
 from pyquil.api import WavefunctionSimulator, QVMConnection
 
+def waveFunction(p):
+    wf_sim = WavefunctionSimulator()
+    waveFunction = wf_sim.wavefunction(p)
+    print(waveFunction)
+    
 class Alice(Agent): 
     def run(self): 
         # Define Alice's qubits
@@ -33,11 +38,13 @@ class Bob(Agent):
         p.if_then(ro[1], X(b))
         p.if_then(ro[0], Z(b))
 
+        waveFunction(p)
         qvm = QVMConnection()
         result = qvm.run(p)
 
 # Create Phi
-p = Program(H(2)) 
+p = Program(H(2))
+waveFunction(p)
 # Entangle qubits 0 and 1. 
 p += Program(H(0))
 p += CNOT(0,1) 
@@ -46,8 +53,8 @@ p += CNOT(0,1)
 ro = p.declare('ro', 'BIT', 3)
 
 # Create Alice and Bob. Give Alice qubit 0 (ancilla) and qubit 2 (phi). Give Bob qubit 1
-alice = Alice(p, [0, 2], 'alice')
-bob = Bob(p, [1], 'bob')
+alice = Alice(p, [0, 2], [], 'alice')
+bob = Bob(p, [1], [], 'bob')
 
 # Connect Alice and Bob via a quantum connection and classical connection with no transit devices
 QConnect(alice, bob, transit_devices=[None])
