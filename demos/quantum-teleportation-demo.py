0b36c021bc2561eb3f0a8e1885f44db384ee4647
zacespinosa <zespinosa97@gmail.com>
2019-06-27 15:41:30 -0700

    move qvm out of bob

--- demos/quantum-teleportation-demo.py
+++ demos/quantum-teleportation-demo.py
@@ -3,10 +3,10 @@ import sys
 sys.path.insert(0, '/Users/zacespinosa/Foundry/qnetdes')
 sys.path.insert(1, '/Users/matthewradzihovsky/documents/qnetdes')
 
-from qnetdes import *
 from pyquil import Program
-from pyquil.gates import *
 from pyquil.api import WavefunctionSimulator, QVMConnection
+from pyquil.gates import *
+from qnetdes import *
 
 def printWF(p):
     wf_sim = WavefunctionSimulator()
@@ -18,6 +18,7 @@ class Alice(Agent):
         # Define Alice's qubits
         a, phi = self.qubits
         p = self.program
+
         # Entangle Ancilla and Phi
         p += CNOT(phi, a)
         p += H(phi)
@@ -39,13 +40,10 @@ class Bob(Agent):
         p.if_then(ro[1], X(b))
         p.if_then(ro[0], Z(b))
 
-        printWF(p)
-        qvm = QVMConnection()
-        result = qvm.run(p)
-
 # Create Phi
 p = Program(H(2))
 printWF(p)
+
 # Entangle qubits 0 and 1. 
 p += Program(H(0))
 p += CNOT(0,1) 
@@ -54,12 +52,16 @@ p += CNOT(0,1)
 ro = p.declare('ro', 'BIT', 3)
 
 # Create Alice and Bob. Give Alice qubit 0 (ancilla) and qubit 2 (phi). Give Bob qubit 1
-alice = Alice(p, [0, 2], [], 'alice')
-bob = Bob(p, [1], [], 'bob')
+alice = Alice(p, [0, 2], 'alice')
+bob = Bob(p, [1], 'bob')
 
 # Connect Alice and Bob via a quantum connection and classical connection with no transit devices
 QConnect(alice, bob, transit_devices=[None])
 CConnect(alice, bob)
+
+qvm = QVMConnection()
+qvm.run(p)
+printWF(p)
 
 # Run simulation
 Simulation(alice, bob).run()
