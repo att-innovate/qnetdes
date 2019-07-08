54527115bf4b668627f8ed07c621506b0003195d
mattradz <mattradz@stanford.edu>
2019-07-08 12:59:03 -0700

    Devices

--- qnetdes/devices.py
+++ qnetdes/devices.py
@@ -43,8 +43,68 @@ class Fiber(Device):
                 print('Apply Fiber Error')
                 ro = program.declare('fiber' + str(uuid.uuid1().int), 'BIT', 1)
                 program += MEASURE(qubit, ro) 
+        delay = self.length/signal_speed
+        return delay
+
+
+class Laser(Device):
+    def __init__(self, XZVariance, wavelength=1550, apply_error=True):
+        self.variance = XZVariance
+        self.wavelength = wavelength
+        
+    def apply(self, program, qubits):
+        for qubit in qubits:
+            if qubit is not None:
+                x_angle, z_angle = np.randoom.normal(0,self.variance,2)
+                program += RX(x_angle, qubit)
+                program += RZ(z_angle, qubit)
+        delay = 0
+        return delay
+
+
+class SNSPD(Device):
+    def __init__(self,  apply_error=True):
+
+        decibel_loss = length*attenuation_coefficient
+        self.attenuation = 10 ** (decibel_loss / 10)
+        self.apply_error = apply_error
+        self.length = length
+
+        
+    def apply(self, program, qubits):
+        for qubit in qubits:
+            if 1 > self.attenuation and qubit is not None:
+                print('using this device', 'fiber' + str(uuid.uuid1().int))
+                identifier = np.random.randint(1,10000)
+                ro = program.declare('fiber' + str(uuid.uuid1().int), 'BIT', 1)
+                program += MEASURE(qubit, ro) 
         delay = self.length
         return delay
+
+
+class Intensity_Modulator(Device):
+    def __init__(self, Average_Photons=1.0, apply_error=True):
+
+        decibel_loss = length*attenuation_coefficient
+        self.attenuation = 10 ** (decibel_loss / 10)
+        self.apply_error = apply_error
+        self.length = length
+
+        
+    def apply(self, program, qubits):
+        for qubit in qubits:
+            if 1 > self.attenuation and qubit is not None:
+                print('using this device', 'fiber' + str(uuid.uuid1().int))
+                identifier = np.random.randint(1,10000)
+                ro = program.declare('fiber' + str(uuid.uuid1().int), 'BIT', 1)
+                if self.apply_error:
+                    program += MEASURE(qubit, ro) 
+        delay = self.length
+        return delay
+
+
+
+
 
         
 
